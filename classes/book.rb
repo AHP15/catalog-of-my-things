e95533d8d1935035adae0c0e6ba1e6a68873352c
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-14 10:14:39 +0500

    Initialize constructor and add a require method

--- classes/book.rb
+++ classes/book.rb
@@ -1,1 +1,14 @@
-# comment to satisfy the linters
+require_relative './item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    (super || cover_state == "bad") ? true : false
+  end
+end
