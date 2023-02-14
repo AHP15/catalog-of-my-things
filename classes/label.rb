90dda33393ddcc3125a43b2ff8ac78372b9e4aa2
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-14 10:57:52 +0500

    Initialize constructor and add a require method

--- classes/label.rb
+++ classes/label.rb
@@ -1,1 +1,14 @@
-# comment to satisfy the linters
+class label
+  attr_accessor :title, :color
+  attr_reader :items
+  def initialize(title, color)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.set_label(self)
+  end
+end
