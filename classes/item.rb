415d9317009c176eb91e62de393affce40a30b69
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-15 19:41:57 +0500

    Add the required methods

--- classes/item.rb
+++ classes/item.rb
@@ -1,19 +1,31 @@
+require 'date'
+
 class Item
-  def initialize(genre, author, source, label, publish_date)
+  attr_reader :genre, :label, :source, :author, :publish_date, :archived
+
+  def initialize(publish_date)
     @id = Random.rand(1..1000)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
     @publish_date = publish_date
     @archived = false
   end
 
-  def can_be_archived?
-    Date.today - @publish_date >= 10.years.ago.to_date
-  end
-
   def move_to_archive
     @archived = true unless can_be_archived?
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_item(self) unless genre.items.include?(self)
+  end
+
+  # def label=(label)
+  #   @label = label
+  #   label.add_item(self) unless label.items.include?(self)
+  # end
+
+  private
+
+  def can_be_archived?
+    Date.today.year - Date.strptime(@publish_date, '%Y/%m/%d').year >= 10
   end
 end
