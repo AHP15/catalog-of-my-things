4311d53c6016454c386393388fbece72f0201118
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-02-16 14:55:08 +0500

    Add the author class code

--- classes/author.rb
+++ classes/author.rb
@@ -1,1 +1,25 @@
-# comment to satisfy the linters
+class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :items, :id
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.author(self)
+  end
+
+  def to_json(*_args)
+    {
+      'id' => @id,
+      'first_name' => @first_name,
+      'last_name' => @last_name,
+      'class' => self.class.name
+    }
+  end
+end
